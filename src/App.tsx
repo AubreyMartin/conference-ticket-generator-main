53421898e94529e75ebd49f04c9613a45b6093c0
AubreyMartin <aubreymartin9000@gmail.com>
2025-03-06 20:42:52 +0530

    Updated UI enhancements and responsive adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import TicketForm from "./components/TicketForm";
 import Ticketgenerater from "./components/Ticketgenerater";
+import BackgroundPatterns from "./components/BackgroundPatterns";
 
 function App() {
   return (
     <Router>
       <Header />
+      <BackgroundPatterns />
       <Routes>
         <Route path="/" element={<TicketForm />} />
         <Route path="/ticket" element={<Ticketgenerater />} />
